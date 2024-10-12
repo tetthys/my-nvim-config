d4ab0cb601ed5f8f6503af2233ea0106ee297e78
tetthys <jeongwoo0830z@gmail.com>
2024-10-12 10:08:14 +0000

    add neo-tree

--- init.lua
+++ init.lua
@@ -38,7 +38,17 @@ require("lazy").setup({
           highlight = { enable = true },
           indent = { enable = true },  
         })
-    end}
+    end},
+		{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+}
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
