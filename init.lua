c2be0aa81b368613ebde3b739f98eaefceccffa7
tetthys <jeongwoo0830z@gmail.com>
2024-10-12 10:18:21 +0000

    add a keymap for neo-tree

--- init.lua
+++ init.lua
@@ -68,3 +68,6 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Use a keymap for neo-tree
+vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left<CR>')
