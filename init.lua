b71c452b0b86dfcd77ae62fdf61412a6e2515fce
tetthys <jeongwoo0830z@gmail.com>
2024-10-12 01:16:12 +0000

    apply cappuccin theme

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- add your plugins here
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -32,3 +32,6 @@ require("lazy").setup({
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+
+-- Use default cappuccin themee
+vim.cmd.colorscheme "catppuccin"
