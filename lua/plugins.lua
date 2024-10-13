ba9cdb20d1626ad67cb41bff5009458e59e7d8ee
tetthys <jeongwoo0830z@gmail.com>
2024-10-13 09:51:08 +0000

    modularized treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,16 +1,6 @@
 
 return {
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = function () 
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-	ensure_installed = { "lua" },
-	sync_install = false,
-	highlight = { enable = true },
-	indent = { enable = true },  
-      })
-  end},
-	      {
+  	      {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
