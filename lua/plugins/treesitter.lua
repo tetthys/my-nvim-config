7bba6dda9af4bcd8861cf8c09964ac18c5072bb6
tetthys <jeongwoo0830z@gmail.com>
2024-10-15 10:47:57 +0000

    enable auto_install on treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,13 @@
-return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = function () 
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-	ensure_installed = { "lua" },
-	sync_install = false,
-	highlight = { enable = true },
-	indent = { enable = true },  
-      })
-  end}
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
