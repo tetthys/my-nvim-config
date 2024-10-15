d6df374272b838c7a12256f9084048f4c95e91db
tetthys <jeongwoo0830z@gmail.com>
2024-10-15 10:55:54 +0000

    use eslint_d and prettier

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,6 +6,8 @@ return {
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.completion.spell,
+                null_ls.builtins.diagnostics.eslint_d,
+                null_ls.builtins.formatting.prettier
             },
         })
 
