0e4380e6476d52d21cf6046e298e274a0a650489
tetthys <jeongwoo0830z@gmail.com>
2024-12-11 20:12:23 +0900

    Show hidden files except .git folder

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,23 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<F2>", ":Neotree filesystem reveal left<CR>")
-	end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    keys = {
+        { "<F2>", ":Neotree filesystem reveal left<CR>", desc = "Open Neo-tree" },
+    },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                hide_dotfiles = false,
+                hide_gitignored = true,
+                hide_by_name = { ".git" },
+            },
+        },
+    },
 }
+
