f51b99a8be0f8a3de2de492cac0f047f9548deec
tetthys <jeongwoo0830z@gmail.com>
2024-12-12 21:18:12 +0900

    fix eslint_d issue

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
     "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
     config = function()
         local null_ls = require("null-ls")
 
@@ -7,8 +10,10 @@ return {
             sources = {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.completion.spell,
-                null_ls.builtins.diagnostics.eslint_d,
+                require("none-ls.diagnostics.eslint_d"),
                 null_ls.builtins.formatting.prettier,
+                null_ls.builtins.diagnostics.phpcs, -- php
+                null_ls.builtins.formatting.phpcsfixer,
             },
         })
 
